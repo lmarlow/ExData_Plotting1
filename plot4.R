eb412716ebc11b4ea97d7e8dfedf9c871d2aea07
Lee Marlow <lee.marlow@gmail.com>
2014-12-06 00:48:25 -0700

    Use `lines` instead of `points` for clarity

--- plot4.R
+++ plot4.R
@@ -15,12 +15,12 @@ with(powerData, {
   plot(datetime, Sub_metering_1,
        type='n',
        ylab='Energy sub metering', xlab='')
-  points(datetime, Sub_metering_1,
-         type='l', col='black')
-  points(datetime, Sub_metering_2,
-         type='l', col='red')
-  points(datetime, Sub_metering_3,
-         type='l', col='blue')
+  lines(datetime, Sub_metering_1,
+        col='black')
+  lines(datetime, Sub_metering_2,
+        col='red')
+  lines(datetime, Sub_metering_3,
+        col='blue')
   legend("topright", legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
          lty=c(1,1,1), col=c('black', 'red', 'blue'), bty='n')
   
